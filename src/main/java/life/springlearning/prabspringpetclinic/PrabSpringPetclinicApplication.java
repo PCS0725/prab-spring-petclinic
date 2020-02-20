4d1f150c2956a9744c12cedc1e9bbbae71c73618
Prabhat Sharma <prabbhat25199@gmail.com>
2020-02-20 16:36:51 +0530

    Initial Project upload

---
+++ src/main/java/life/springlearning/prabspringpetclinic/PrabSpringPetclinicApplication.java
@@ -0,0 +1,13 @@
+package life.springlearning.prabspringpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrabSpringPetclinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrabSpringPetclinicApplication.class, args);
+    }
+
+}
