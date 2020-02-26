6968ef7d3ae26bc9e83568ad395bffcfa0bfb8ea
Prabhat Sharma <prabbhat25199@gmail.com>
2020-02-26 21:26:47 +0530

    plugin modifications2

---
+++ pet-clinic-data/src/main/java/life/springlearning/prabspringpetclinic/services/OwnerService.java
@@ -0,0 +1,4 @@
+package life.springlearning.prabspringpetclinic.services;
+
+public interface OwnerService {
+}
--- pet-clinic-web/src/test/java/life/springlearning/prabspringpetclinic/PrabSpringPetclinicApplicationTests.java
+++ pet-clinic-web/src/test/java/life/springlearning/prabspringpetclinic/PrabSpringPetclinicApplicationTests.java
@@ -1,9 +1,14 @@
 package life.springlearning.prabspringpetclinic;
 
+import life.sprinlearning.prabspringpetclinic.PrabSpringPetclinicApplication;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = PrabSpringPetclinicApplication.class)
 class PrabSpringPetclinicApplicationTests {
 
     @Test
